41f4fc6afc1ed3e3e4a04edccb7b6c560c604f23
jcolep <89608819+jcolep@users.noreply.github.com>
2022-04-14 21:11:24 -0600

    dropdown test

--- script.js
+++ script.js
@@ -2,5 +2,18 @@
  * Shows menu contents when menu button is clicked
  */
 function menuButtonClicked() {
-    document.getElementById("menuDropdown").classList.toggle("show");
+    document.getElementById("dropdownMenu").classList.toggle("show");
+}
+
+window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+        var dropdowns = document.getElementsByClassName("dropdown-content");
+        var i;
+        for (i = 0; i < dropdowns.length; i++) {
+            var openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains('show')) {
+                openDropdown.classList.remove('show');
+            }
+        }
+    }
 }
